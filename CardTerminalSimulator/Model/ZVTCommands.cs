63f7d2eb11d0e4966aa60b8f9d7b105869d08232
Bernd Schuhmacher <bernd.schuhmacher@cranimax.com>
2023-09-18 11:44:35 +0200

    chore: Added class for ZVTCommands

---
+++ CardTerminalSimulator/Model/ZVTCommands.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+
+namespace CardTerminalSimulator.Model;
+
+public sealed class ZvtCommands : Hashtable
+{
+    public ZvtCommands()
+    {
+        Add("CommandCompletion", new byte[] { 0x80, 0x00, 0x00 });
+        Add("Completion", new byte[] { 0x06, 0x0F, 0x00 });
+        Add("EndOfDay", new byte[] { 0x06, 0x50 });
+    }
+}
